a4213edd26db93db741c065e29ea73bf439d1e85
Octavio Llanillo <llanillo4@hotmail.com>
2022-09-04 23:45:56 -0300

    Update home UI

--- Scripts/UI/HomeUI.gd
+++ Scripts/UI/HomeUI.gd
@@ -4,7 +4,7 @@ onready var spawn_bomber_position := $SpawnBomberPosition
 onready var start_button := $VBox/Button
 onready var spawn_timer := $SpawnTimer
 
-export (PackedScene) var bomber_scene := preload("res://Scenes/UI/BomberDecoration.tscn")
+export (PackedScene) var bomberman_decoration__scene := preload("res://Scenes/UI/BomberDecoration.tscn")
 
 
 
@@ -14,13 +14,14 @@ func _ready():
 	spawn_timer.start()
 
 
+
 func on_start_button_press() -> void:
 	SceneChanger.switch_to_next_level(true)
 
 
 
 func on_spawn_timer_timeout() -> void:
-	var bomber_instance := bomber_scene.instance()
+	var bomber_instance := bomberman_decoration__scene.instance()
 	bomber_instance.global_position = spawn_bomber_position.global_position
 	add_child(bomber_instance)
 	spawn_timer.start()
