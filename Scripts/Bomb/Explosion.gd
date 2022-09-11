7cda2d3dce018a0b196d28ecb7d178fc025959c6
Octavio Llanillo <llanillo4@hotmail.com>
2022-09-11 11:23:04 -0300

    Update Explosion.gd

--- Scripts/Bomb/Explosion.gd
+++ Scripts/Bomb/Explosion.gd
@@ -1,26 +1,22 @@
 extends Node
 
-onready var destroy_timer := $DestroyTimer as Timer
-
-var explosion_duration_time : float
+onready var animation_player := $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered", self, "on_body_entered")
-	destroy_timer.connect("timeout", self, "on_explosion_timer_timeout")	
-	destroy_timer.set_one_shot(true)
-	destroy_timer.start(explosion_duration_time)
+	animation_player.play("explosion")
+	AudioManager.play_exposion()
 
 
 func on_body_entered(body: Node) -> void:
 	if body.is_in_group("Player"):
-		var player := body as PlayerManager
-		EventManager.emit_signal("player_die", player.player_type)		
-	
+		EventManager.emit_signal("player_die", body.player_type)		
+		
 	if body.is_in_group("Bomb"):
 		body.init_bomb_explosion()
 		
 		
 
-func on_explosion_timer_timeout() -> void:
+func queue_free_explosion() -> void:
 	call_deferred("queue_free")
