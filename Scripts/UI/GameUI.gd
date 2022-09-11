566f503f94f20213ae073a7fd9ec8accd4246a39
Octavio Llanillo <llanillo4@hotmail.com>
2022-09-11 11:22:42 -0300

    Replace sprites placeholders

--- Scripts/UI/GameUI.gd
+++ Scripts/UI/GameUI.gd
@@ -1,15 +1,15 @@
 extends Control
 
 # Player 1 related canvas
-onready var player1_bomb_count := $Player1/VBox/Bomb/GrayArea/Count as Label
-onready var player1_fire_count := $Player1/VBox/Fire/GrayArea/Count as Label
-onready var player1_label := $Player1/VBox/PlayerLabel as Label
+onready var player1_bomb_count := $Player1/Bomb/GrayArea/Count as Label
+onready var player1_fire_count := $Player1/Fire/GrayArea/Count as Label
+onready var player1_label := $Player1/Label as Label
 onready var player1_panel := $Player1 as Panel
 
 # Player 2 related canvas
-onready var player2_bomb_count := $Player2/VBox/Bomb/GrayArea/Count as Label
-onready var player2_fire_count := $Player2/VBox/Fire/GrayArea/Count as Label
-onready var player2_label := $Player2/VBox/PlayerLabel as Label
+onready var player2_bomb_count := $Player2/Bomb/GrayArea/Count as Label
+onready var player2_fire_count := $Player2/Fire/GrayArea/Count as Label
+onready var player2_label := $Player2/Label as Label
 onready var player2_panel := $Player2 as Panel
 
 
