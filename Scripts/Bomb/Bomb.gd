51b7e9fc717b9a4a07eabb54175e7d71dbee6a5c
Octavio Llanillo <llanillo4@hotmail.com>
2022-09-13 19:59:02 -0300

    Fix bomb/player collision bug

--- Scripts/Bomb/Bomb.gd
+++ Scripts/Bomb/Bomb.gd
@@ -31,15 +31,22 @@ func init_bomb_explosion() -> void:
 	instantiate_explosion(global_position)
 	for direction in direction_array:
 		instantiate_explosion_line(global_position, direction)
-	
+
 	call_deferred("queue_free")
 	
 
 
 func on_body_exited(body: Node) -> void:	
+	# Enables bomb rigidbody only if there are no players in the area2d
 	if body.is_in_group("Player"):
-		area2d_collision_shape.set_deferred("disabled", true)
-		rigidbody_collision_shape.set_deferred("disabled", false)
+		var is_there_a_player_in_bomb_area = false
+		for object in area2d.get_overlapping_bodies():
+			if object.is_in_group("Player"):
+				is_there_a_player_in_bomb_area = true
+		
+		if !is_there_a_player_in_bomb_area:
+			area2d_collision_shape.set_deferred("disabled", true)
+			rigidbody_collision_shape.set_deferred("disabled", false)
 	
 	
 
@@ -65,5 +72,4 @@ func instantiate_explosion_line(position: Vector2, direction: Vector2) -> void:
 func instantiate_explosion(position) -> void:	
 	var explosion_instance = explosion_scene.instance()
 	explosion_instance.global_position = position	
-#	explosion_instance.explosion_duration_time = explosion_duration
 	get_tree().root.call_deferred("add_child", explosion_instance)	
