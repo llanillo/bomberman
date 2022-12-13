2bc221cc2cc6cbeb086bfa747617e5c692a9c4af
Octavio Llanillo <llanillo4@hotmail.com>
2022-12-12 22:28:58 -0300

    Update PropertyUtil.cs

--- Scripts/Utils/PropertyUtil.cs
+++ Scripts/Utils/PropertyUtil.cs
@@ -4,9 +4,12 @@ public static class PropertyUtil
 {
     public const string PlayerGroup = "Player";
     
-    public const string BodyExited = "body_exited";
-    public const string TimeOut = "timeout";
+    public const string DestructibleBrickGroup = "DBrick";
+    public const string IndestructibleBrickGroup = "UBrick";
+    
 
     public const string Disabled = "disabled";
     public const string AddChildren = "add_child";
+    
+    public const string Collider = "collider";
 }
