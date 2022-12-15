647047c960687262c410acc5f68b3ece7587d8df
Octavio Llanillo <llanillo4@hotmail.com>
2022-12-14 22:12:23 -0300

    Update SignalUtil.cs

--- Scripts/Utils/SignalUtil.cs
+++ Scripts/Utils/SignalUtil.cs
@@ -2,6 +2,7 @@
 
 public static class SignalUtil
 {
+    public const string BodyEntered = "body_entered";
     public const string BodyExited = "body_exited";
     public const string TimeOut = "timeout";
     
