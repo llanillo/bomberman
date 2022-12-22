6daaa1e4319963190bce489785c8c459c06b2acf
Octavio Llanillo <llanillo4@hotmail.com>
2022-12-22 16:37:38 -0300

    Update EventManager.cs

--- Scripts/System/EventManager.cs
+++ Scripts/System/EventManager.cs
@@ -1,20 +1,19 @@
-﻿namespace Bomberman.System
+namespace Bomberman.System;
+
+public class EventManager : Node
 {
-    public class EventManager : Node
-    {
-        [Signal]
-        public delegate void BombExplosion(PlayerIndex playerType);
+    [Signal]
+    public delegate void BombExplosion(PlayerIndex playerType);
 
-        [Signal]
-        public delegate void PlayerDie(PlayerIndex playerType);
+    [Signal]
+    public delegate void PlayerDie(PlayerIndex playerType);
 
-        [Signal]
-        public delegate void ItemPickup(PlayerIndex playerType, ItemType itemType);
+    [Signal]
+    public delegate void ItemPickup(PlayerIndex playerType, ItemType itemType);
 
-        [Signal]
-        public delegate void SuddenDeathStart();
+    [Signal]
+    public delegate void SuddenDeathDestroyBricks();
 
-        [Signal]
-        public delegate void SuddenDeathDestroyBricks();
-    }
+    [Signal]
+    public delegate void SuddenDeathStart();
 }
